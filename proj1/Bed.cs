b37f7292d003f00b2c4cbfa85e253dde475cb85c
EduardHas <93647839+EduardHas@users.noreply.github.com>
2022-04-20 20:22:30 +0300

    Add project files.

---
+++ proj1/Bed.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj1
+{
+    public class Bed
+    {
+        //מאפיינים של פריט מיטה 
+        public string model;
+        public int height;
+        public int width;
+        public float price;
+        public bool isInStock = true;//ברירת מחדל של משתנה 
+
+
+        //הגדרת פעולויות /פונקציות ששיכות למחלקה מיטה 
+        public void MakeOrder()
+        {
+            height = 0;
+        }
+        public int DrugDown()
+        {
+            return 0;
+        }
+             
+
+    }
+}
---
+++ proj1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {   
+            //מופע =אובייקט
+            Bed b1 = new Bed();
+            Bed b2 = new Bed();
+            Bed b3 = new Bed();
+          
+
+            //יצירת שלש מופעים  שונים
+            b1.model = "Sapapa";
+            b1.price = 55f;
+
+            b2.model = "Sababa";
+            b2.price = 600f;
+
+            b3.model = "Bamaba";
+            b3.price = 23f;
+
+
+
+            Console.ReadLine();
+
+          
+        }
+    }
+}
